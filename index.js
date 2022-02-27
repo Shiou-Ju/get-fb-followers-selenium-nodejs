5fe57d1468fb8782311e83a79215179b0db11985
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2022-02-27 22:54:44 +0800

    feat: implement fb login function

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-require("dotenv").config;
+require("dotenv").config("./.env");
 
 const webDriver = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const path = require("path");
 const fs = require("fs");
+const { setTimeout } = require("timers/promises");
 
 const filePath = "./chromedriver";
 const driverPath = path.join(__dirname, filePath);
@@ -27,9 +28,14 @@ const checkDriver = () => {
   }
 };
 
-const openCrawler = async () => {
+const loginFacebook = async () => {
   const isDriverOk = checkDriver();
   if (isDriverOk) return;
+
+  const { until, By } = webDriver;
+
+  const userName = process.env.FB_USER_NAME;
+  const passWord = process.env.FB_PASS_WORD;
 
   try {
     const serviceBuilder = new chrome.ServiceBuilder();
@@ -38,14 +44,31 @@ const openCrawler = async () => {
       .setChromeService(serviceBuilder)
       .build();
 
-    const url = "http://google.com";
+    const url = "https://www.facebook.com/login";
+
     await driver.get(url);
-    await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
+
+    const emailXpath = `//*[@id="email"]`;
+    const pwXpath = `//*[@id="pass"]`;
+    const loginXpath = `//*[@id="loginbutton"]`;
+
+    const emailElement = await driver.wait(
+      until.elementLocated(By.xpath(emailXpath))
+    );
+    const pwElement = await driver.wait(
+      until.elementLocated(By.xpath(pwXpath))
+    );
+    const loginBtn = await driver.wait(
+      until.elementLocated(By.xpath(loginXpath))
+    );
+
+    await emailElement.sendKeys(userName);
+    await pwElement.sendKeys(passWord);
+
+    await loginBtn.click();
   } catch (error) {
     console.error(error);
-    console.error("can not establish a new browser");
-    return;
   }
 };
 
-openCrawler();
+loginFacebook();
