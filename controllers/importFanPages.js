52abd569fd890eb59915e77f00c0841d920906c9
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2022-02-28 11:03:36 +0800

    refactor: withdraw importFanPages

---
+++ controllers/importFanPages.js
@@ -0,0 +1,15 @@
+const importFanPages = () => {
+  const fanPages = [
+    {
+      fanPageName: '一起讀判決',
+      fanPageUrl: 'https://www.facebook.com/legal.taiwan',
+    },
+    {
+      fanPageName: '市宅街冰店',
+      fanPageUrl: 'https://www.facebook.com/amabingdien/',
+    },
+  ];
+  return fanPages;
+};
+
+module.exports = importFanPages;
--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 require('dotenv').config('./.env');
 
+const importFanPages = require('./controllers/importFanPages')
 const initDriver = require('./controllers/initDriver');
 const loginFacebook = require('./controllers/loginFacebook');
 const goToFanPage = require('./controllers/goToFanPage');
 const getFollowersNumber = require('./controllers/getFollowersNumber');
-
-const importFanPages = () => {
-  const fanPages = [
-    {
-      fanPageName: '一起讀判決',
-      fanPageUrl: 'https://www.facebook.com/legal.taiwan',
-    },
-    {
-      fanPageName: '市宅街冰店',
-      fanPageUrl: 'https://www.facebook.com/amabingdien/',
-    },
-  ];
-  return fanPages;
-};
 
 const main = async () => {
   const fanPages = importFanPages();
