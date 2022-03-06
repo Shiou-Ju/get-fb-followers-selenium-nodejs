cc373f6d4354fb773f0af5aa8fceb8d1b98e594c
Shiou-Ju <75402531+Shiou-Ju@users.noreply.github.com>
2022-03-06 16:04:08 +0800

    feat: commander option to hide images

--- controllers/initDriver.js
+++ controllers/initDriver.js
@@ -3,6 +3,27 @@ require('dotenv').config('./.env');
 const webDriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const checkDriver = require('./checkDriver');
+const { program: commander } = require('commander');
+
+const getCommanderOpts = () => {
+  commander.option('--hide-images', '不要顯示網站的圖片', false);
+  commander.parse();
+  return commander.opts();
+};
+
+const customizeOptions = () => {
+  const { hideImages } = getCommanderOpts();
+  const isHideImages = !!hideImages;
+
+  const options = new chrome.Options();
+
+  // hide images
+  if (isHideImages) {
+    options.addArguments('--blink-settings=imagesEnabled=false');
+  }
+
+  return options;
+};
 
 const initDriver = async () => {
   const isDriverOk = checkDriver();
@@ -13,9 +34,12 @@ const initDriver = async () => {
 
   try {
     const serviceBuilder = new chrome.ServiceBuilder();
+    const options = customizeOptions();
+
     const driver = await new webDriver.Builder()
       .forBrowser('chrome')
       .setChromeService(serviceBuilder)
+      .setChromeOptions(options)
       .build();
 
     return driver;
